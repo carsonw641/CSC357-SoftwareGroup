7af80675397dff088147630bb34db09e291429ec
Devin Deneault <devdeneault@gmail.com>
2018-10-10 11:06:08 -0600

    Added socket test code

---
+++ code-samples/socket.py
@@ -0,0 +1,14 @@
+import socket
+
+#98:d3:31:fd:94:46
+
+serverMACAddress = '98:d3:31:fd:94:46'
+port = 3
+s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+s.connect((serverMACAddress,port))
+while 1:
+    text = input()
+    if text == "quit":
+        break
+    s.send(bytes(text, 'UTF-8'))
+s.close()
